63a44bde65f23d614553e4c15fa13564e5099bde
佐藤 由介 <yusukesato@satounoMacBook-Air.local>
2025-03-28 15:01:44 +0900

    必要なクラスのファイル作成

---
+++ src/Server.cpp
@@ -0,0 +1,1 @@
+ExecuteServer()
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
+#include "Server.hpp"
+#include "ASocket.hpp"
+#include "ListenSocket.hpp"
+#include "CgiSocket.hpp"
+#include "ClientSocket.hpp"
+#include "Request.hpp"
+#include "Response.hpp"
+
 int main(int argc, char* argv[]) {
   if (argc != 2) {
     return 1;
   }
   return 0;
+  try {
+    // 設定ファイルの情報をもらう
+    ParseConfig();
+    ASocket listen_socket;
+    CreateListenServer();
+    ExecuteServer();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "エラーが発生しました。"<< e.what() << std::endl;
+  }
 }
